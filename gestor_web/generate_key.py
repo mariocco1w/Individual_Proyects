8a1b702a634eac8ecddb27c6fc5d7ecd8a8c712b
mariocco1w <151478643+mariocco1w@users.noreply.github.com>
2026-02-04 11:22:23 -0600

    Delete gestor_web directory

--- gestor_web/generate_key.py
+++
@@ -1,6 +0,0 @@
-from cryptography.fernet import Fernet
-
-with open('clave.key.nueva', 'wb') as f:
-    f.write(Fernet.generate_key())
-
-print('Nueva clave generada y guardada en clave.key.nueva') 
